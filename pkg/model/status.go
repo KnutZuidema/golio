7993c88205ec96524bb6d1511fc6ec9d6c9bdf38
Knut Zuidema <knut.zuidema@gmail.com>
2019-05-11 00:39:27 +0200

    add status models

--- pkg/model/status.go
+++ pkg/model/status.go
@@ -12,4 +12,29 @@ type Status struct {
 type Service struct {
 	Status    string     `json:"status"`
 	Incidents []Incident `json:"incidents"`
+	Name      string     `json:"name"`
+	Slug      string     `json:"slug"`
+}
+
+type Incident struct {
+	Active    bool            `json:"active"`
+	CreatedAt string          `json:"created_at"`
+	ID        int             `json:"id"`
+	Updates   []StatusMessage `json:"updates"`
+}
+
+type StatusMessage struct {
+	Severity     string              `json:"severity"`
+	Author       string              `json:"author"`
+	CreatedAt    string              `json:"created_at"`
+	Translations []StatusTranslation `json:"translations"`
+	UpdatedAt    string              `json:"updated_at"`
+	Content      string              `json:"content"`
+	ID           string              `json:"id"`
+}
+
+type StatusTranslation struct {
+	Locale    string `json:"locale"`
+	Content   string `json:"content"`
+	UpdatedAt string `json:"updated_at"`
 }
