36f970f3a253d21ef6aa852762db3c2f430c2fc7
Knut Zuidema <k.zuidema@gridx.de>
2019-05-14 18:11:41 +0200

    use added constructors in client constructor

--- pkg/api/client.go
+++ pkg/api/client.go
@@ -11,15 +11,7 @@ type Client struct {
 
 func NewClient(region region, apiKey string, client *http.Client) *Client {
 	return &Client{
-		RiotAPIClient: &RiotAPIClient{
-			region: region,
-			apiKey: apiKey,
-			client: client,
-		},
-		DataDragonClient: &DataDragonClient{
-			DataDragonVersion:  "9.9.1",
-			DataDragonLanguage: LanguageCodeUnitedStates,
-			client:             client,
-		},
+		RiotAPIClient:    NewRiotAPIClient(region, apiKey, client),
+		DataDragonClient: NewDataDragonClient(client, region),
 	}
 }
