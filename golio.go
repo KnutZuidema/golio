e5f807fcbcd84607447eb7af5d4e3cb5fe53e706
Knut Zuidema <me@knutzuidema.de>
2020-02-11 14:34:12 +0100

    feat(golio): use option based approach for configuration of new client

--- golio.go
+++ golio.go
@@ -2,23 +2,27 @@
 // idiomatic access to all API endpoints.
 //
 // Example:
-//	 client := golio.NewClient(api.RegionEuropeWest, "API KEY", http.DefaultClient, log.StandardLogger())
-//	 summoner, _ := client.Riot.GetSummonerByName("SK Jenax")
-//	 fmt.Printf("%s is a level %d summoner\n", summoner.Name, summoner.SummonerLevel)
-//	 champion, _ := client.DataDragon.GetChampion("Ashe")
-//	 mastery, err := client.Riot.GetChampionMastery(summoner.ID, champion.Key)
-//	 if err != nil {
-//	 	fmt.Printf("%s has not played any games on %s\n", summoner.Name, champion.Name)
-//	 } else {
-//	 	fmt.Printf("%s has mastery level %d with %d points on %s\n", summoner.Name, mastery.ChampionLevel,
-//	 		mastery.ChampionPoints, champion.Name)
-//	 }
-//	 challengers, _ := client.Riot.GetChallengerLeague(api.QueueRankedSolo)
-//	 rank1 := challengers.GetRank(0)
-//	 fmt.Printf("%s is the highest ranked player with %d league points\n", rank1.SummonerName, rank1.LeaguePoints)
+//   client := golio.NewClient("API KEY",
+//                 golio.WithRegion(api.RegionNorthAmerica),
+//                 golio.WithLogger(logrus.New().WithField("foo", "bar")))
+//   summoner, _ := client.Riot.GetSummonerByName("SK Jenax")
+//   fmt.Printf("%s is a level %d summoner\n", summoner.Name, summoner.SummonerLevel)
+//   champion, _ := client.DataDragon.GetChampion("Ashe")
+//   mastery, err := client.Riot.GetChampionMastery(summoner.ID, champion.Key)
+//   if err != nil {
+//   fmt.Printf("%s has not played any games on %s\n", summoner.Name, champion.Name)
+//   } else {
+//   fmt.Printf("%s has mastery level %d with %d points on %s\n", summoner.Name, mastery.ChampionLevel,
+//   mastery.ChampionPoints, champion.Name)
+//   }
+//   challengers, _ := client.Riot.GetChallengerLeague(api.QueueRankedSolo)
+//   rank1 := challengers.GetRank(0)
+//   fmt.Printf("%s is the highest ranked player with %d league points\n", rank1.SummonerName, rank1.LeaguePoints)
 package golio
 
 import (
+	"net/http"
+
 	log "github.com/sirupsen/logrus"
 
 	"github.com/KnutZuidema/golio/api"
@@ -30,16 +34,52 @@ import (
 
 // Client is a client for both the Riot API and the Data Dragon service
 type Client struct {
+	client     internal.Doer
+	logger     log.FieldLogger
+	region     api.Region
+	apiKey     string
 	Riot       *riot.Client
 	DataDragon *datadragon.Client
 	Static     *static.Client
 }
 
-// NewClient returns a new client for both the Riot API and the Data Dragon service
-func NewClient(region api.Region, apiKey string, client internal.Doer, logger log.FieldLogger) *Client {
-	return &Client{
-		Riot:       riot.NewClient(region, apiKey, client, logger),
-		DataDragon: datadragon.NewClient(client, region, logger),
-		Static:     static.NewClient(client, logger),
+// Option is used to alter the attributes of a client
+type Option func(*Client)
+
+// WithClient sets the given http client for the golio client
+func WithClient(c internal.Doer) Option {
+	return func(client *Client) {
+		client.client = c
 	}
+}
+
+// WithLogger sets the given logger for the golio client
+func WithLogger(l log.FieldLogger) Option {
+	return func(client *Client) {
+		client.logger = l
+	}
+}
+
+// WithRegion sets the given region for the golio client
+func WithRegion(r api.Region) Option {
+	return func(client *Client) {
+		client.region = r
+	}
+}
+
+// NewClient returns a new client for both the Riot API and the Data Dragon service
+func NewClient(apiKey string, options ...Option) *Client {
+	c := &Client{
+		client: http.DefaultClient,
+		logger: log.StandardLogger(),
+		region: api.RegionEuropeWest,
+		apiKey: apiKey,
+	}
+	for _, opt := range options {
+		opt(c)
+	}
+	c.Riot = riot.NewClient(c.region, c.apiKey, c.client, c.logger)
+	c.DataDragon = datadragon.NewClient(c.client, c.region, c.logger)
+	c.Static = static.NewClient(c.client, c.logger)
+	return c
 }
