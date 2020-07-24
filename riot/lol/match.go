844671ac9532a41bf4184432ee0be2b90d325d08
ironpark <cjfdhksaos@gmail.com>
2020-07-25 07:34:15 +0900

    feat(riot/lol): add option parameter to match client List()

--- riot/lol/match.go
+++ riot/lol/match.go
@@ -2,9 +2,9 @@ package lol
 
 import (
 	"fmt"
-	"io"
-
 	log "github.com/sirupsen/logrus"
+	"io"
+	"strconv"
 
 	"github.com/KnutZuidema/golio/internal"
 )
@@ -12,6 +12,35 @@ import (
 // MatchClient provides methods for the match endpoints of the League of Legends API.
 type MatchClient struct {
 	c *internal.Client
+}
+
+// MatchListOptions
+type MatchListOptions struct {
+	// Set of champion IDs for filtering the matchlist.
+	Champion int
+	// Set of queue IDs for filtering the matchlist.
+	Queue int
+	// The begin time to use for filtering matchlist specified as epoch milliseconds.
+	BeginTime int64
+	// The end time to use for filtering matchlist specified as epoch milliseconds.
+	EndTime int64
+}
+
+func (mo *MatchListOptions) buildParam() string {
+	var param string
+	if mo.Champion != 0 {
+		param += "&champion=" + strconv.Itoa(mo.Champion)
+	}
+	if mo.Queue != 0 {
+		param += "&queue=" + strconv.Itoa(mo.Queue)
+	}
+	if mo.BeginTime != 0 {
+		param += "&beginTime=" + strconv.FormatInt(mo.BeginTime, 10)
+	}
+	if mo.EndTime != 0 {
+		param += "&endTime=" + strconv.FormatInt(mo.EndTime, 10)
+	}
+	return param
 }
 
 // Get returns a match specified by its ID
@@ -26,13 +55,14 @@ func (m *MatchClient) Get(id int) (*Match, error) {
 }
 
 // List returns a specified range of matches played on the account
-func (m *MatchClient) List(accountID string, beginIndex, endIndex int) (*Matchlist, error) {
+func (m *MatchClient) List(accountID string, beginIndex, endIndex int, options ...*MatchListOptions) (*Matchlist, error) {
 	logger := m.logger().WithField("method", "List")
 	var matches *Matchlist
-	if err := m.c.GetInto(
-		fmt.Sprintf(endpointGetMatchesByAccount, accountID, beginIndex, endIndex),
-		&matches,
-	); err != nil {
+	endpoint := fmt.Sprintf(endpointGetMatchesByAccount, accountID, beginIndex, endIndex)
+	if len(options) != 0 {
+		endpoint += options[0].buildParam()
+	}
+	if err := m.c.GetInto(endpoint, &matches); err != nil {
 		logger.Debug(err)
 		return nil, err
 	}
