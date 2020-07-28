69213c02c22d3fd09104017e826ba571d2a18f61
ironpark <cjfdhksaos@gmail.com>
2020-07-29 00:02:36 +0900

    fix(match_test gofmt) lint pass

--- riot/lol/match_test.go
+++ riot/lol/match_test.go
@@ -38,7 +38,7 @@ func TestMatchClient_List(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			client := internal.NewClient(api.RegionEuropeWest, "API_KEY", tt.doer, logrus.StandardLogger())
-			got, err := (&MatchClient{c: client}).List("id", 0, 1,&MatchListOptions{
+			got, err := (&MatchClient{c: client}).List("id", 0, 1, &MatchListOptions{
 				Champion:  []int{1},
 				Queue:     []int{200},
 				BeginTime: time.Time{},
