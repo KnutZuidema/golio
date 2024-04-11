1bce0c4b7da4f918608bf228137d61631cf35cea
Harmon Herring <harmonherring@gmail.com>
2024-04-10 21:31:05 -0400

    implement accountclient functions directly on client

--- riot/account/account.go
+++ riot/account/account.go
@@ -2,20 +2,14 @@ package account
 
 import (
 	"fmt"
+
 	"github.com/KnutZuidema/golio/api"
 
 	log "github.com/sirupsen/logrus"
-
-	"github.com/KnutZuidema/golio/internal"
 )
 
-// AccountClient provides methods for the account endpoints of the League of Legends API.
-type AccountClient struct { //nolint:golint
-	c *internal.Client
-}
-
 // GetByPUUID returns the account matching the PUUID
-func (ac *AccountClient) GetByPUUID(puuid string) (*Account, error) {
+func (ac *Client) GetByPUUID(puuid string) (*Account, error) {
 	logger := ac.logger().WithField("method", "GetByPUUID")
 	var account Account
 	c := *ac.c
@@ -32,7 +26,7 @@ func (ac *AccountClient) GetByPUUID(puuid string) (*Account, error) {
 }
 
 // GetByRiotID returns the account matching the riot id
-func (ac *AccountClient) GetByRiotID(gameName, tagLine string) (*Account, error) {
+func (ac *Client) GetByRiotID(gameName, tagLine string) (*Account, error) {
 	logger := ac.logger().WithField("method", "GetByRiotID")
 	var account Account
 	c := *ac.c
@@ -48,6 +42,6 @@ func (ac *AccountClient) GetByRiotID(gameName, tagLine string) (*Account, error)
 	return &account, nil
 }
 
-func (ac *AccountClient) logger() log.FieldLogger {
+func (ac *Client) logger() log.FieldLogger {
 	return ac.c.Logger().WithField("category", "account")
 }
--- riot/account/client.go
+++ riot/account/client.go
@@ -4,12 +4,12 @@ import "github.com/KnutZuidema/golio/internal"
 
 // Client pools all methods for endpoints of the League of Legends API.
 type Client struct {
-	AccountClient *AccountClient
+	c *internal.Client
 }
 
 // NewClient returns a new instance of a League of Legends client.
 func NewClient(base *internal.Client) *Client {
 	return &Client{
-		AccountClient: &AccountClient{base},
+		base,
 	}
 }
